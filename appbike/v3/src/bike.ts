743562f103e480d8c224820aa947e6ba1e6ca496
Gabriel Machado <104879073+hrZero@users.noreply.github.com>
2023-09-11 20:26:30 -0300

    Add files via upload

---
+++ appbike/v3/src/bike.ts
@@ -0,0 +1,14 @@
+export class Bike {
+    constructor(
+        public name: string,
+        public type: string,
+        public bodySize: number,
+        public maxLoad: number,
+        public rate: number,
+        public description: string,
+        public ratings: number,
+        public imageUrls: string[],
+        public available: boolean = true,
+        public id?: string
+    ) {}
+}
